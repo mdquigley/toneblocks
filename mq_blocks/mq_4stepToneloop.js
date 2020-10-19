0ff94988b533bbbef24255c3232fd2b6f794b294
mike <quiginfo@gmail.com>
2020-10-19 16:24:53 -0400

    Test for ChangePanVol

--- mq_blocks/mq_4stepToneloop.js
+++ mq_blocks/mq_4stepToneloop.js
@@ -56,7 +56,9 @@ Blockly.JavaScript['mq_4stepToneloop'] = function (block) {
             let ${synth}Seq = new Tone.Loop((time) => {
                 ${synth}ChangeType();
                 //${synth}AmpEnv();
-                ${synth}ChangePanVol();
+                if (panvols['${synth}']) {
+                    ${synth}ChangePanVol();
+                }
                 updateInterval(${synth}Seq, subdivisions['${synth}']);
                 if (sequences['${synth}'][${synth}Counter % 4] !== null) {
                     ${synth}.triggerAttackRelease(((eval(sequences['${synth}'][${synth}Counter % 4])) ? Tone.Frequency(eval(sequences['${synth}'][${synth}Counter % 4]), "midi") : null), '8n');
